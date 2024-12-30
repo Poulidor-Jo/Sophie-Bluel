a9d143723675538eb784dba7c440dd1ea862ce4b
Jo <jordanmoret4@gmail.com>
2024-12-30 13:02:30 +0100

    get works done

--- js/app.js
+++ js/app.js
@@ -3,26 +3,25 @@ async function getWorks() {
     try {
         const response = await fetch(url);
         if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
+            throw new Error(`Erreur de réponse : ${response.status}`);
         }
 
         const json = await response.json();
-        console.log(json);
-        for (let i = 0; i < json.lenght; i++) {
-            setFigure(json[i])
+        console.log(json); // Pour voir les données reçues
+        for (let i = 0; i < json.length; i++) {
+            setFigure(json[i]);
         }
-    }
-    catch(error) {
-        console.error(error.message);
+    } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error.message);
     }
 }
 
-getWorks();
-
 function setFigure(data) {
     const figure = document.createElement("figure");
-    figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
+    figure.innerHTML = `<img src="${data.imageUrl}" alt="${data.title}">
                         <figcaption>${data.title}</figcaption>`;
 
     document.querySelector(".gallery").append(figure);
 }
+
+getWorks();
