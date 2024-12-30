77913256cfe479e5333fafb29b136200baf65660
Jo <jordanmoret4@gmail.com>
2024-12-30 12:06:21 +0100

    get works

---
+++ js/app.js
@@ -0,0 +1,28 @@
+async function getWorks() {
+    const url = "http://localhost:5678/api/works";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+        for (let i = 0; i < json.lenght; i++) {
+            setFigure(json[i])
+        }
+    }
+    catch(error) {
+        console.error(error.message);
+    }
+}
+
+getWorks();
+
+function setFigure(data) {
+    const figure = document.createElement("figure");
+    figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
+                        <figcaption>${data.title}</figcaption>`;
+
+    document.querySelector(".gallery").append(figure);
+}
