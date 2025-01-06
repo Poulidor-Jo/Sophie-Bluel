26f59c218802886f1e33c3f9d54c67731b0febe8
Jo <jordanmoret4@gmail.com>
2025-01-06 15:06:17 +0100

    add categories

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
-async function getWorks() {
-    const url = "http://localhost:5678/api/works";
+let allWorks = []
+let allCategories= []
+
+const getData = async (collection) => {
+    const url = `http://localhost:5678/api/${collection}`;
     try {
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error(`Erreur de réponse : ${response.status}`);
         }
 
-        const json = await response.json();
-        console.log(json); // Pour voir les données reçues
-        for (let i = 0; i < json.length; i++) {
-            setFigure(json[i]);
-        }
+        const data = await response.json();
+        console.log("retour data", data); // Valeur data
+        return data;
     } catch (error) {
         console.error("Erreur lors de la récupération des données :", error.message);
     }
@@ -24,27 +25,18 @@ function setFigure(data) {
     document.querySelector(".gallery").append(figure);
 }
 
-getWorks();
-/*
-async function getCategories () {
-    const url = "http://localhost:5678/api/categories";
-    try {
-        const response = await fecth(url);
-        if (!response.ok){
-            throw new Error(`Response status: {response.status}`);
-        }
 
-        const json = await response.json();
-        for (let i = 0; i < json; i++){
-            setFilter(json[i]);
-        }
-    }
-    catch (error) {
-        console.error(error.message);
-    }
+const init = async() => {
+    const works = await getData("works")
+    console.log(works)
+    allWorks = works;
+    const categories = await getData("categories")
+    console.log(categories)
+    allCategories = categories;
+
 }
 
-getCategories(data){
-    const di
-}
-*/
+init().then(() => {
+    allWorks.forEach((work) => setFigure(work))
+    console.log("allCategories", allCategories)
+})
